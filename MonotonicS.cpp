69971016ac39a6c97ec4e1fb1988251e339cbc8b
Priya Chanchal <145918494+Priya-C-016@users.noreply.github.com>
2025-05-14 20:32:14 +0530

    Create MonotonicS.cpp

---
+++ MonotonicS.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> monotonicIncreasing(vector<int>nums){
+    int n=nums.size();
+    stack<int>st;
+    vector<int>result(n,-1);
+    for(int i = 0; i < n; i++) {
+    while(!st.empty() && nums[i] < nums[st.top()]) {
+        result[st.top()] = nums[i];
+        st.pop();
+    }
+    st.push(i);
+}
+return result;
+}
+
+
+vector<int> nextGreaterElement(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> result(n, -1);  // default: -1 if no greater element exists
+    stack<int> st;  // stores indices of elements
+
+    for (int i = 0; i < n; i++) {
+        // Check if current element is greater than stack's top
+        while (!st.empty() && nums[i] > nums[st.top()]) {
+            result[st.top()] = nums[i];  // Found next greater for index on top
+            st.pop();
+        }
+        st.push(i);  // Push current index to stack
+    }
+
+    return result;
+}
+
+
+
+int main(){
+     vector<int> nums = {3, 1, 4, 1, 5, 9, 2, 6};
+    vector<int> result = monotonicIncreasing(nums);
+    cout << "Monotonic increasing stack: ";
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout << endl;
+    vector<int> ans = nextGreaterElement(nums);
+    
+    for (int val : ans)
+        cout << val << " ";
+    return 0;
+}
