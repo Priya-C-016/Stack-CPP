2bfad8687e55bac9067e3306df8d251f6a66a698
Priya Chanchal <145918494+Priya-C-016@users.noreply.github.com>
2025-05-18 22:53:02 +0530

    Create InfixToPostfix.cpp

---
+++ InfixToPostfix.cpp
@@ -0,0 +1,57 @@
+int prec(char c) {
+    if (c == '^')
+        return 3;
+    else if (c == '/' || c == '*')
+        return 2;
+    else if (c == '+' || c == '-')
+        return 1;
+    else
+        return -1;
+}
+    string infixToPostfix(string& s) {
+        // code here
+        stack<char> st;
+    string res;
+
+    for (int i = 0; i < s.length(); i++) {
+        char c = s[i];
+
+        // If the scanned character is
+        // an operand, add it to the output string.
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))
+            res += c;
+
+        // If the scanned character is an
+        // ‘(‘, push it to the stack.
+        else if (c == '(')
+            st.push('(');
+
+        // If the scanned character is an ‘)’,
+        // pop and add to the output string from the stack
+        // until an ‘(‘ is encountered.
+        else if (c == ')') {
+            while (st.top() != '(') {
+                res += st.top();
+                st.pop();
+            }
+            st.pop(); 
+        }
+
+        // If an operator is scanned
+        else {
+            while (!st.empty() && prec(c) <= prec(st.top())) {
+                res += st.top();
+                st.pop();
+            }
+            st.push(c);
+        }
+    }
+
+    // Pop all the remaining elements from the stack
+    while (!st.empty()) {
+        res += st.top();
+        st.pop();
+    }
+
+    return res;
+    }
